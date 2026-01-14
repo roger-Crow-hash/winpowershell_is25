9aa668292dffa6d4fb663071536ea5c55630a76e
Roger-Andreas Pedak <roger-andreas.pedak@voco.ee>
2026-01-14 14:45:15 +0200

    Practice 2 - Hashtable

---
+++ Practice 2.ps1
@@ -0,0 +1,10 @@
+﻿$hashtable1=@{Name="John","Joe","Mary"; DaysWorked=12,20,18}
+$hashtable2=@{Name="John","Joe","Mary"; SalaryPerDay=100,120,150}
+
+$johnsalary = $hashtable1.DaysWorked[0] * $hashtable2.SalaryPerDay[0]
+$joesalary = $hashtable1.DaysWorked[1] * $hashtable2.SalaryPerDay[1]
+$marysalary = $hashtable1.DaysWorked[2] * $hashtable2.SalaryPerDay[2]
+
+$hashtable3=@{Name=$hashtable1.Name[0],$hashtable1.name[1],$hashtable1.name[2]; Salary=$johnsalary,$joesalary,$marysalary}
+
+$hashtable3
