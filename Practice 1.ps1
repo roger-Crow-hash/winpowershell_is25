c628d64c52e53d94764f470ddb0fb9d8776b265d
Roger-Andreas Pedak <roger-andreas.pedak@voco.ee>
2026-01-14 13:49:18 +0200

    Practice 1 - Array

---
+++ Practice 1.ps1
@@ -0,0 +1,9 @@
+﻿$Array1 = @("1","2","3")
+$Array2 = @("4","5","6")
+$Array3 = @()
+
+$Array3 += $Array1[0] + $Array2[0]
+$Array3 += $Array1[1] + $Array2[1]
+$Array3 += $Array1[2] + $Array2[2]
+
+$Array3
